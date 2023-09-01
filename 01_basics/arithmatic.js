7f2b4ec5f9fab1b01fb1271420f6951483c9c696
Zahid Kirmani <shahzaahid038@gmail.com>
2023-09-01 22:28:25 +0530

    arithmatic etc operations

---
+++ 01_basics/arithmatic.js
@@ -0,0 +1,24 @@
+let num = 10;
+let num2 = -num;
+console.log(num2);
+
+console.log(2 + 2);
+console.log(2 * 2);
+console.log(2 / 2);
+console.log(2 - 2);
+console.log(2 ** 3);
+console.log(10 % 3);
+console.log("-----------------------------------------------\n\n");
+//with  before double quote it is going to get as it is "
+console.log("2" + 2);
+console.log("2" + 2 + "\t" + 2 + "2" + "\t" + 2 + 2);
+console.log("2" + 2 + "\t" + 2 + "2" + "\t" + 2 + 2 + "\t" + 2 + "2");
+
+console.log(2 + 2 + "2" + "3" + "4" + 2 + 4);
+
+let isLoggedIn = true;
+console.log(isLoggedIn);
+
+let num1, nume2, num3;
+num1 = num2 = num3 = num4 = 2 + 2;
+console.log(num1);
---
+++ 01_basics/incrDecr.js
@@ -0,0 +1,5 @@
+let num1 = 20;
+console.log(++num1);
+// The answer will be 20 because of pre-increment
+console.log(++num1);
+// The answer will be 22 because of post incremtn
