fa1d03936a6061ccf57cfc5c4e5ad4a46984ad20
Zahid Kirmani <shahzaahid038@gmail.com>
2023-10-28 16:22:03 +0530

    correction

--- projects/project2/index.js
+++ projects/project2/index.js
@@ -1,13 +1,15 @@
-const form = document.querySelector('form');
-form.addEventListener('submit', function (e) {
+const form = document.querySelector("form");
+form.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  const height = parseInt(document.querySelector('#height').value);
-  const weight = parseInt(document.querySelector('#weight').value);
-  const results = document.querySelector('#results');
+  const height = parseInt(document.querySelector("#height").value);
+  const weight = parseInt(document.querySelector("#weight").value);
+  const results = document.querySelector("#results");
 
-  if (height === '' || height < 0 || isNaN(height) || weight === '' || weight < 0 || isNaN(weight)) {
-    results.innerHTML = 'Please give valid height and weight';
+  if (height === "" || height < 0 || isNaN(height)) {
+    results.innerHTML = "Please give valid height";
+  } else if (weight === "" || weight < 0 || isNaN(weight)) {
+    results.innerHTML = "Please give valid weight";
   } else {
     const bmi = (weight / ((height * height) / 10000)).toFixed(2);
 
