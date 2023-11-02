d519a9d67e3666c6ff1d3b637eeb9c6a0d5e5160
Zahid Kirmani <shahzaahid038@gmail.com>
2023-11-02 11:03:07 +0530

    Improve the display the text

--- projects/project1/index.js
+++ projects/project1/index.js
@@ -1,21 +1,27 @@
 const buttons = document.querySelectorAll(".button");
 const body = document.querySelector("body");
 const text = document.querySelector("h1");
-
 buttons.forEach(function (button) {
   button.addEventListener("click", function (e) {
+    console.log(e);
     if (e.target.id == "grey") {
       body.style.backgroundColor = e.target.id;
+      text.innerHTML = `Color Scheme Switcher (${e.target.id})`;
     } else if (e.target.id == "blue") {
       body.style.backgroundColor = e.target.id;
+      text.innerHTML = `Color Scheme Switcher (${e.target.id})`;
     } else if (e.target.id == "yellow") {
       body.style.backgroundColor = e.target.id;
+      text.innerHTML = `Color Scheme Switcher (${e.target.id})`;
     } else if (e.target.id == "orange") {
       body.style.backgroundColor = e.target.id;
+      text.innerHTML = `Color Scheme Switcher (${e.target.id})`;
     } else if (e.target.id == "white") {
       body.style.backgroundColor = "white";
+      text.innerHTML = `Color Scheme Switcher (${e.target.id})`;
     } else {
       body.style.backgroundColor = "purple";
+      text.innerHTML = `Color Scheme Switcher (purple)`;
     }
   });
 });
