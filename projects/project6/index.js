b25631d568c480dce74cd79d7dc1a2ab42169afd
Zahid Kirmani <shahzaahid038@gmail.com>
2023-11-08 19:18:22 +0530

    add projects

---
+++ projects/project6/index.js
@@ -0,0 +1,24 @@
+const randomNumber = function () {
+  const hex = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+let intervalId;
+const startChangingColor = function () {
+  if (!intervalId) {
+    intervalId = setInterval(changeBgColor, 1000);
+  }
+  function changeBgColor() {
+    document.body.style.background = randomNumber();
+  }
+};
+const stopChaingingColor = function () {
+  clearInterval(intervalId);
+  intervalId = null;
+};
+
+document.querySelector("#start").addEventListener("click", startChangingColor);
+document.querySelector("#stop").addEventListener("click", stopChaingingColor);
